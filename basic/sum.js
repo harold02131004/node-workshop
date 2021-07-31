3342af5346cf9a91f570e9ca42ad91c0471990c3
harold <harold02131004@gmail.com>
2021-07-31 13:47:10 +0800

    add

---
+++ basic/sum.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+function sum(n){
+    let result = 0;
+
+    for (i = 1; i<=n ; i++){
+        result += i;
+        
+    } 
+    return result;
+}
+
+console.log(sum(10)); 
