47139f43398ad0083873aed9100a1572b6301749
harold <harold02131004@gmail.com>
2021-08-06 17:35:42 +0800

    add  promise.js and Async-Await-promise.js

--- promise/promise.js
+++ promise/promise.js
@@ -29,8 +29,8 @@
 
 
   
-//   let dt = new Date();
-//   console.log(`開始工作 at ${dt.toISOString()}`);
+  let dt = new Date();
+  console.log(`開始工作 at ${dt.toISOString()}`);
 //   // 刷牙 -> 吃早餐 -> 寫功課
   
 //   // 解決: 接續做的工作
@@ -63,16 +63,33 @@
 //   });
   
 
-  let job1 = doWork("刷牙", 3000, true);
+  let job1 = doWork("刷牙", 1000, true);
 console.log(job1);
-job1.then(
-  function (resolve) {
-    console.log("第 1 個函式被呼叫了", resolve);
-  },
-  function (reject) {
-    console.log("第 2 個函式被呼叫了", reject);
-  }
-);
+job1
+.then((resolve)=>{
+
+    console.log( "刷牙成功了",resolve);
+    return doWork("吃早餐",5000,true);
+ })
+ .then((resolve)=>{
+
+    console.log( "吃早餐成功了",resolve);
+    return doWork("寫功課",5000,true);
+ })
+ .then((resolve)=>{
+
+    console.log( "寫功課成功了",resolve);
+   
+ })
+ .catch((error) =>{
+    console.log( "失敗",error);
+ })
+.finally(() => {
+    console.log("最後結果");
+});
+
+
+
 
 //  let job2 = doWork("吃早餐", 3000, true);
 // console.log(job2);
