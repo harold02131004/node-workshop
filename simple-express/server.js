efd53651e9a6c83d0eab31979804d2bb14b79315
harold <harold02131004@gmail.com>
2021-08-15 11:35:42 +0800

    新增middleware

--- simple-express/server.js
+++ simple-express/server.js
@@ -2,11 +2,28 @@ const express = require("express");
 
 let app = express();
 
+
+app.use((req, res, next)=>{
+    let current = new Date();
+   console.log(`123來訪問瞜 at ${current.toISOString()}`);
+    next();
+  });
+
+
+
+
 //http Method :get post put
 
 app.get("/",function (request , response, next){
     response.send("Hello");
 });
+
+
+app.use((req, res, next)=>{
+    
+    console.log(123);
+     next();
+   });
 
 app.get("/about",function (request , response, next){
     response.send("about us");
@@ -16,5 +33,6 @@ app.get("/about",function (request , response, next){
 app.listen(3000, function () {
     console.log("我們的 web server 啟動了～");
   });
+
 
 
